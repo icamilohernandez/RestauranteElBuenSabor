c8e15b89913ade853f88e078e0d234800a0c49bd
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-04-19 23:21:08 -0500

    feat: Solucion de el punto 4 para las clases Proceso y Utilidades

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
@@ -3,27 +3,11 @@ package com.mycompany.restauranteelbuensabor;
 public class Proceso {
 
     public static double calcularSubtotal() {
-        double subtotal = 0;
-        int indice = 0;
-        while (indice < Datos.nombres.length) {
-            if (Datos.cantidades[indice] > 0) {
-                subtotal = subtotal + Datos.precios[indice] * Datos.cantidades[indice];
-            }
-            indice++;
-        }
-        return subtotal;
+        return Datos.pedidoActual.calcularSubtotal();
     }
 
     public static int contarItemsPedido() {
-        int contador = 0;
-        int indice = 0;
-        while (indice < Datos.cantidades.length) {
-            if (Datos.cantidades[indice] > 0) {
-                contador = contador + 1;
-            }
-            indice++;
-        }
-        return contador;
+        return Datos.pedidoActual.contarItemsDiferentes();
     }
 
     public static double aplicarDescuento(double subtotal) {
@@ -45,31 +29,10 @@ public class Proceso {
     }
 
     public static double calcularTotal() {
-        double subtotal              = calcularSubtotal();
-        double subtotalConDescuento  = aplicarDescuento(subtotal);
-        double iva                   = calcularIVA(subtotalConDescuento);
-        double totalConIva           = subtotalConDescuento + iva;
-        double propina               = calcularPropina(subtotalConDescuento);
-        double total                 = totalConIva + propina;
+        Factura factura = new Factura(Datos.pedidoActual, Datos.numeroFactura);
+        double total = factura.calcularTotal();
         Datos.estadoMesa = 1;
         Datos.total = total;
         return total;
-    }
-
-    public static double procesar(double precio, double cantidad, double descuento, double tasaIva, double tasaPropina, int numeroItems, boolean aplicaPropina) {
-        double resultado = precio * cantidad;
-        if (descuento > 0) {
-            resultado = resultado - (resultado * descuento);
-        }
-        double iva = resultado * tasaIva;
-        resultado = resultado + iva;
-        if (aplicaPropina) {
-            double propina = resultado * tasaPropina;
-            resultado = resultado + propina;
-        }
-        if (numeroItems > Datos.MIN_ITEMS_DESCUENTO) {
-            resultado = resultado - (resultado * 0.01);
-        }
-        return resultado;
     }
 }
--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Utilidades.java
@@ -3,26 +3,13 @@ package com.mycompany.restauranteelbuensabor;
 public class Utilidades {
 
     public static boolean validar() {
-        int contador = 0;
-        int indice = 0;
-        while (indice < Datos.cantidades.length) {
-            if (Datos.cantidades[indice] > 0) {
-                contador = contador + 1;
-            }
-            indice++;
-        }
-        return contador > 0;
+        return Datos.pedidoActual.tieneProductos();
     }
 
     public static void reiniciar() {
-        int indice = 0;
-        while (indice < Datos.cantidades.length) {
-            Datos.cantidades[indice] = 0;
-            indice++;
-        }
+        Datos.pedidoActual.limpiar();
         Datos.total = 0;
         Datos.estadoMesa = 0;
         Datos.numeroMesaActual = 0;
-        Datos.textoTemporal = "";
     }
 }
