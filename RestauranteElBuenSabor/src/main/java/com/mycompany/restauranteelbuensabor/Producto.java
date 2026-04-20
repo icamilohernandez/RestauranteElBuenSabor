9198a45024fae56a9b8e92d375289943526aeab4
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-04-19 22:54:23 -0500

    feat: creacion de la clase Producto

---
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Producto.java
@@ -0,0 +1,14 @@
+package com.mycompany.restauranteelbuensabor;
+
+public class Producto {
+    private final String nombre;
+    private final double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+}
