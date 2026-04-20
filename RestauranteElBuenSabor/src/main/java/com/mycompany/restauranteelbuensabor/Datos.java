88327108b8a0bfe35aef23dc9fadc809d9f54960
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-04-19 23:11:23 -0500

    feat: solucion punto 4 en la clase datos

--- RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
+++ RestauranteElBuenSabor/src/main/java/com/mycompany/restauranteelbuensabor/Datos.java
@@ -10,13 +10,20 @@ public class Datos {
     public static final String DIRECCION           = "Calle 15 #8-32, Valledupar";
     public static final String NIT                 = "900.123.456-7";
 
-    public static String[] nombres = { "Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa",
-            "Cerveza Poker", "Agua Panela", "Arroz con Pollo" };
-    public static double[] precios = { 32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000 };
-    public static int[] cantidades = { 0, 0, 0, 0, 0, 0, 0, 0 };
-    public static int numeroMesaActual = 0;
-    public static int estadoMesa = 0;
-    public static double total = 0;
-    public static int numeroFactura = 1;
-    public static String textoTemporal = "";
+    public static final Producto[] CARTA = {
+        new Producto("Bandeja Paisa",       32000),
+        new Producto("Sancocho de Gallina", 28000),
+        new Producto("Arepa con Huevo",      8000),
+        new Producto("Jugo Natural",         7000),
+        new Producto("Gaseosa",              4500),
+        new Producto("Cerveza Poker",        6000),
+        new Producto("Agua Panela",          3500),
+        new Producto("Arroz con Pollo",     25000)
+    };
+
+    public static int    numeroMesaActual = 0;
+    public static int    estadoMesa       = 0;
+    public static double total            = 0;
+    public static int    numeroFactura    = 1;
+    public static Pedido pedidoActual     = new Pedido();
 }
